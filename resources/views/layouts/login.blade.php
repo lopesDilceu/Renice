6b314321aae235d1eb2209523972d8c9226494ae
Dilceu Lopes <dilceulopesjr181@gmail.com>
2024-06-11 09:47:06 -0300

    alterações

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -27,11 +27,11 @@
         @yield('content')
     </div>
     <footer>
-        Desenvolvido por <a href="https://github.com/lopesDilceu">Dilceu Lopes</a>&copy;
+        Desenvolvido por &copy;<a href="https://github.com/lopesDilceu" target="_blank">Dilceu Lopes</a>
     </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    
+    <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
     @stack('script')
 </body>
 
